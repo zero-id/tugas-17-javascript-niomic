d59792d220b35b074b081bf17201f240ec36a75c
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-13 17:37:46 +0700

    tugas 17 js

---
+++ tugas17.js
@@ -0,0 +1,11 @@
+var kata1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk."
+var kata2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual"
+
+console.log(kata1.toLowerCase())
+console.log(kata1.toUpperCase())
+
+var kalimat3 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+var enam = kalimat3.substr(0, 6);
+
+console.log(enam);
+console.log(enam.charCodeAt(0), "-", enam.charCodeAt(1), "-", enam.charCodeAt(2), "-", enam.charCodeAt(3), "-", enam.charCodeAt(4), "-", enam.charCodeAt(5));
